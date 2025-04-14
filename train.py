f4a49c29eb3e80bcb247a06c2e4f5190f2198a7d
thibault-roux <thibault.roux@uclouvain.be>
2025-04-14 14:50:47 +0200

    Fix bugs

--- train.py
+++ train.py
@@ -5,7 +5,9 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import jiwer
 
 
 # Image Captioning Model
@@ -40,17 +42,20 @@ class ImageCaptioningModel(nn.Module):
 
 
 # Function to Load Data
-def get_training_data(lang='fr', set_type='train', sentence_key='sentence1'):
+def get_training_data(acronym='stsb', info_name='fr', set_type='train', sentence_key='sentence1', adder='', fullconcat=False):
     '''
     Load the training data from the saved embeddings
     Args:
-        lang: str, language of the dataset
+        info_name: str, Language or wikitext type of the dataset
         set_type: str, type of the dataset (train, dev, test)
-        sentence_key: str, key of the sentence in the dataset
+        sentence_key: str, key of the sentence in the dataset (could be sentence1, sentence2, paragraph)
     Returns:
         id2embedding: dict, mapping of sentence ID to a tuple (text, embeddings)
     '''
-    saved_embeddings_path = f"/globalscratch/ucl/cental/troux/corpus/stsb/embeddings_{lang}_{set_type}_{sentence_key}.pkl"
+    if fullconcat:
+        saved_embeddings_path = f"/globalscratch/ucl/cental/troux/corpus/{acronym}/embeddings_{info_name}_{set_type}_wiki_stsb{adder}.pkl"
+    else:
+        saved_embeddings_path = f"/globalscratch/ucl/cental/troux/corpus/{acronym}/embeddings_{info_name}_{set_type}_{sentence_key}{adder}.pkl"
     if os.path.exists(saved_embeddings_path):
         print(f"Embeddings already exist at {saved_embeddings_path}. Loading...")
         with open(saved_embeddings_path, "rb") as f:
@@ -64,12 +69,14 @@ def get_training_data(lang='fr', set_type='train', sentence_key='sentence1'):
 # Custom Dataset Class
 class CaptionDataset(Dataset):
     def __init__(self, data, tokenizer, max_length=30):
-        self.data = list(data.items())  # Convert dict to list of tuples (id, (text, embedding))
+        # self.data = list(data.items())  # Convert dict to list of tuples (id, (text, embedding)) # deprecated due to change in data format
+        # data # list of tuples (text, embedding))
+        self.data = [(idx, item) for idx, item in enumerate(data)]  # Add index to each item, transform
         self.tokenizer = tokenizer
         self.max_length = max_length
 
     def __len__(self):
-        return len(self.data) - 1 # DON'T FORGET TO REMOVE THE -1 AFTER EXPERIMENTING -------------------------------------------- 
+        return len(self.data)
 
     def __getitem__(self, idx):
         text, embedding = self.data[idx][1]  # Get caption & embedding
@@ -83,14 +90,31 @@ class CaptionDataset(Dataset):
         return embedding_tensor, input_ids, attention_mask
 
 # Training Function
-def train(model, dataloader, optimizer, criterion, device, epochs=5):
+def train(model, train_dataloader, valid_dataloader, optimizer, criterion, device, epochs=5):
     model.train()
     model.to(device)
+
+    # if results/best.txt exists, load it
+    if os.path.exists("results/best.txt"):
+        with open("results/best.txt", "r") as f:
+            lines = f.readlines()
+            best_eval_loss = float(lines[-1].split(",")[1].split(":")[1])
+            # Load the model state
+            model.load_state_dict(torch.load("results/models/emb_captioning_best_model.pth"))
+            # Load the optimizer state
+            optimizer.load_state_dict(torch.load("results/emb_captioning_best_optimizer.pth"))
+            print(f"Loaded best model with loss {best_eval_loss:.4f}")
+    else:
+        best_eval_loss = float("inf")
 
     for epoch in range(epochs):
         total_loss = 0
 
-        for image_features, input_ids, _ in dataloader:  # Ignore attention_mask
+        idx_batch = 0
+        # for image_features, input_ids, _ in dataloader:  # Ignore attention_mask
+        for batch in tqdm(train_dataloader, desc=f"Epoch {epoch + 1}/{epochs}", unit="batch"):
+            idx_batch += 1
+            image_features, input_ids, _ = batch
             image_features, input_ids = image_features.to(device), input_ids.to(device)
 
             optimizer.zero_grad()
@@ -101,25 +125,103 @@ def train(model, dataloader, optimizer, criterion, device, epochs=5):
             seq_len = input_ids.size(1) - 1  # Expected target sequence length
             loss = criterion(logits[:, :seq_len, :].reshape(-1, logits.size(-1)), input_ids[:, 1:].reshape(-1))
 
-
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
 
-        avg_loss = total_loss / len(dataloader)
+        avg_loss = total_loss / len(train_dataloader)
         print(f"Epoch {epoch + 1}/{epochs}, Loss: {avg_loss:.4f}")
 
+        # Save model
+        torch.save(model.state_dict(), f"results/models/emb_captioning_epoch_{epoch + 1}.pth")
+        print(f"Model saved at epoch {epoch + 1}")
+
+        # Evaluate the model
+        eval_loss = evaluate(model, valid_dataloader, device)
+        if eval_loss < best_eval_loss:
+            best_eval_loss = eval_loss
+            torch.save(model.state_dict(), "results/models/emb_captioning_best_model.pth")
+            torch.save(optimizer.state_dict(), "results/models/emb_captioning_best_optimizer.pth")
+            print(f"Best model saved with loss {best_eval_loss:.4f}")
+            # Save the best loss
+            with open("results/best.txt", "a") as f:
+                f.write(f"Epoch {epoch + 1}, Loss: {best_eval_loss:.4f}\n")
+        else:
+            print(f"Model not improved, current best loss: {best_eval_loss:.4f}")
 
 
-        # Infer on the last text of the dataset
-        text, embedding = train_data[len(train_data) - 1]
-        tokenized_text = tokenizer(text, return_tensors="pt")
-        input_ids = tokenized_text["input_ids"].to(device)
-        output = model(torch.tensor(embedding).unsqueeze(0).to(device), input_ids)
-        predicted_text = tokenizer.decode(output.argmax(-1).squeeze().tolist())
-        print(f"Predicted: {predicted_text}")
-        print(f"Actual: {text}")
-        print("--------------------")
+        model.train()
+
+
+def evaluate(model, dataloader, device):
+    model.eval()
+    model.to(device)
+    total_loss = 0
+    with torch.no_grad():
+        for batch in tqdm(dataloader, desc="Evaluating", unit="batch"):
+            image_features, input_ids, _ = batch
+            image_features, input_ids = image_features.to(device), input_ids.to(device)
+
+            logits = model(image_features, input_ids[:, :-1])
+            seq_len = input_ids.size(1) - 1
+            loss = criterion(logits[:, :seq_len, :].reshape(-1, logits.size(-1)), input_ids[:, 1:].reshape(-1))
+            total_loss += loss.item()
+
+    avg_loss = total_loss / len(dataloader)
+    print(f"Evaluation Loss: {avg_loss:.4f}")
+    return avg_loss
+
+
+def evaluate_best_model(model, test_data, tokenizer, device):
+    # Load the best model
+    model.load_state_dict(torch.load("results/models/emb_captioning_best_model.pth"))
+    model.to(device)
+
+    # scores
+    wer_scores = []
+    cer_scores = []
+    # Set the model to evaluation mode
+    model.eval()
+
+    # Evaluate the model by computing word error rate (WER) between predictions and ground truth
+    for batch in tqdm(test_data, desc="Evaluating Best Model", unit="batch"):
+        image_features, input_ids, _ = batch
+        image_features, input_ids = image_features.to(device), input_ids.to(device)
+
+        # Generate predictions
+        with torch.no_grad():
+            logits = model(image_features, input_ids[:, :-1])
+            predicted_ids = torch.argmax(logits[:, -1, :], dim=-1)
+
+        # Decode the predicted and ground truth captions
+        predicted_captions = tokenizer.batch_decode(predicted_ids, skip_special_tokens=True)
+        ground_truth_captions = tokenizer.batch_decode(input_ids[:, 1:], skip_special_tokens=True)
+
+        # Compute WER or any other evaluation metric here
+        for pred, gt in zip(predicted_captions, ground_truth_captions):
+            wer = jiwer.wer(gt, pred)
+            cer = jiwer.cer(gt, pred)
+            wer_scores.append(wer)
+            cer_scores.append(cer)
+
+    # Compute average WER and CER
+    wer_avg = sum(wer_scores) / len(wer_scores)
+    cer_avg = sum(cer_scores) / len(cer_scores)
+
+    # Print the results
+    print(f"Average WER: {wer_avg:.4f}")
+    print(f"Average CER: {cer_avg:.4f}")
+    # Save the results
+    with open("results/evaluation_results.txt", "w") as f:
+        f.write(f"Average WER: {wer_avg:.4f}\n")
+        f.write(f"Average CER: {cer_avg:.4f}\n")
+    # Save the predictions
+    with open("results/predictions.txt", "w") as f:
+        for pred, gt in zip(predicted_captions, ground_truth_captions):
+            f.write(f"Predicted: {pred}\n")
+            f.write(f"Ground Truth: {gt}\n")
+            f.write("\n")        
+
 
 
 
@@ -129,13 +231,28 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load Data
-    train_data = get_training_data(lang='fr', set_type='train', sentence_key='concat')
+    train_data = get_training_data(acronym='wikitext', info_name='wikitext-72', set_type='train', sentence_key='paragraph', adder='_split', fullconcat=False)
+    print("before:", type(train_data)) # dict
+    # print first element of the dict
+    print("first element:", list(train_data.items())[0])
+    # split in train, valid and test data with train_test_split
+    train_data, valid_data = train_test_split(train_data, test_size=0.2, random_state=42)
+    print("after:", type(train_data)) # list
+    # print first element of the list
+    print("first element:", train_data[0])
+
+    valid_data, test_data = train_test_split(valid_data, test_size=0.1, random_state=42)
+    
     tokenizer = GPT2Tokenizer.from_pretrained("asi/gpt-fr-cased-base")
     tokenizer.pad_token = tokenizer.eos_token
 
     # Create Dataset & DataLoader
-    dataset = CaptionDataset(train_data, tokenizer)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    train_data = CaptionDataset(train_data, tokenizer)
+    valid_data = CaptionDataset(valid_data, tokenizer)
+    test_data = CaptionDataset(test_data, tokenizer)
+    train_data = DataLoader(train_data, batch_size=100, shuffle=True)
+    valid_data = DataLoader(valid_data, batch_size=100, shuffle=False)
+    test_data = DataLoader(test_data, batch_size=100, shuffle=False)
 
     # Initialize Model, Optimizer & Loss
     model = ImageCaptioningModel(image_embedding_dim=768).to(device)
@@ -143,8 +260,4 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
 
     # Train the Model
-    train(model, dataloader, optimizer, criterion, device, epochs=200)
-
-    # Save the trained model
-    torch.save(model.state_dict(), "results/image_captioning_model.pth")
-    print("Model saved!")
+    train(model, train_data, valid_data, optimizer, criterion, device, epochs=200)
